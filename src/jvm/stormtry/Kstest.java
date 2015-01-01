65f4c8aeff8210c5559de874c9cee9f8335a6837
root <root@master.(none)>
2015-01-01 20:00:22 +0800

    kafka-storm

--- src/jvm/stormtry/Kstest.java
+++ src/jvm/stormtry/Kstest.java
@@ -6,12 +6,16 @@ import backtype.storm.Config;
 import backtype.storm.topology.TopologyBuilder;
 import storm.kafka.SpoutConfig;
 import storm.kafka.KafkaSpout;
-import storm.kafka.KafkaConfig;
+import storm.kafka.ZkHosts;
 import backtype.storm.StormSubmitter;
 import org.apache.storm.guava.collect.ImmutableList;
-
+import backtype.storm.LocalCluster;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Kstest {
+  private static final Logger LOG = LoggerFactory.getLogger(Kstest.class);
+
   private final TopologyBuilder builder;
   private final Config topologyConfig;
   private final String topologyName;
@@ -34,7 +38,17 @@ public class Kstest {
     String boltId = "kafkaBolt";
     List<String> hosts = new ArrayList<String>();
     hosts.add("172.18.14.101");
-    SpoutConfig spoutConf = new SpoutConfig(KafkaConfig.StaticHosts.fromHostString(hosts, 1), "xueyutest", "/xueyukafkastorm", "ksid");
+    //hosts.add("172.18.14.101:9093");
+    //hosts.add("172.18.14.101:9094");
+
+    List<String> zkServers = new ArrayList<String>();
+    zkServers.add("172.18.14.101");
+    /*SpoutConfig spoutConf = new SpoutConfig(KafkaConfig.StaticHosts.fromHostString(hosts, 1), "xueyutest", "/storm", "ksid");
+    spoutConf.zkServers = zkServers;
+    spoutConf.zkPort = 2181;
+    spoutConf.forceStartOffsetTime(-2);*/
+    
+    SpoutConfig spoutConf = new SpoutConfig(new ZkHosts("172.18.14.101:2181", "/brokers"), "kstesttopic", "/mykstest", "ksid");
     /*SpoutConfig spoutConfig = new SpoutConfig(
       ImmutableList.of("172.18.14.101"), // list of Kafka brokers
       1, // number of partitions per host
@@ -51,13 +65,27 @@ public class Kstest {
     StormSubmitter.submitTopology(topologyName, topologyConfig, builder.createTopology());
   }
 
+  public void runLocal() throws Exception {
+    LOG.info("######run Local");
+    LocalCluster cluster = new LocalCluster();
+    cluster.submitTopology(topologyName, topologyConfig, builder.createTopology());
+    Thread.sleep(100000000);
+    cluster.shutdown();
+  }
+
   public static void main(String[] args) throws Exception {
     String topologyName = "KstestTopo";
-    if (args.length >= 1) {
-      topologyName =  args[0];
+    if (args.length >= 2) {
+      topologyName =  args[1];
     }
-    
+
     Kstest kst = new Kstest(topologyName);
-    kst.runRemotely();
+    if (args[0].equals("local")) {
+      kst.runLocal();
+      
+    } else {
+        
+      kst.runRemotely();
+    }
   }
 }
--- src/jvm/stormtry/StubBolt.java
+++ src/jvm/stormtry/StubBolt.java
@@ -4,13 +4,53 @@ import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Tuple;
+import backtype.storm.LocalCluster;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.*;
+import java.util.Map;
+import java.lang.Exception;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class StubBolt extends BaseBasicBolt {
 
+
+  //FileWriter fw;
+  
+
+    /*@Override
+    public Map<String, Object> getComponentConfiguration() {
+            try{
+    fw = new FileWriter("/tmp/storm-test/s1.log");
+    } catch (Exception io) {
+    }
+        return null;
+    } */
+private static final Logger LOG = LoggerFactory.getLogger(StubBolt.class);
+
   @Override
   public void execute(Tuple tuple, BasicOutputCollector collector) {
     System.out.println(tuple);
+    FileWriter fw = null;
+    try {
+      LOG.info("####i am here");
+      Object obj = tuple.getValue(0);
+      LOG.info("##tuple"+tuple.toString());
+      LOG.info("##tuple obj:"+obj.toString());
+      fw = new FileWriter("/tmp/storm-test/s1.log", true);
+      fw.write(obj.toString());
+      fw.write("\n");
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+       fw.close();
+      } catch (Exception e){
+        e.printStackTrace();
+      }
+    }
   }
 
   @Override
